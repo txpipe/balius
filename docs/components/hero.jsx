44ae624f9dd0fbe7375c2d00ea43ba47b8d5d81c
Santiago Carmuega <santiago@carmuega.me>
2024-09-18 09:27:01 -0300

    docs: organize available docs (#20)

--- docs/components/hero.jsx
+++ docs/components/hero.jsx
@@ -2,17 +2,14 @@ export function Hero() {
   return (
     <div className="py-10 md:p-20">
       <div className="mb-5 text-center">
-        <div className="flex justify-center">
-          <img src="/logo.png" alt="Hollow logo" className="mb-4" />
-        </div>
-        <h1 className="flex flex-col flex-wrap font-bold text-4xl md:text-6xl lg:text-7xl dark:text-gray-200">
-          SDK for building dApps
+        <h1 className="flex flex-col flex-wrap font-bold text-4xl md:text-4xl lg:text-6xl dark:text-gray-200">
+          Balius: Headless dApp SDK
         </h1>
       </div>
 
       <div className="mb-8 max-w-3xl text-center mx-auto">
-        <p className="md:text-lg text-gray-600 dark:text-gray-400">
-          Hollow SDK streamlines the development of headless Cardano dApps by providing essential components out-of-the-box. Developers can focus on their business logic while leveraging the SDK's plumbing for on-chain interactions, promoting code reusability and scalability.
+        <p className="md:text-2xl text-gray-600 dark:text-gray-400">
+          Balius SDK streamlines the development of headless dApps for UTxO-based blockchains. It decouples business logic from on-chain interactions, promoting code reusability and scalability.
         </p>
       </div>
 
