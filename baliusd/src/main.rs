da1cffe4ff83d43c78d1d6bba98e5439cc92249a
Santiago Carmuega <santiago@carmuega.me>
2024-10-27 19:31:24 -0300

    feat(baliusd): load worker config from json file (#33)

--- baliusd/src/main.rs
+++ baliusd/src/main.rs
@@ -25,7 +25,7 @@ pub struct WorkerConfig {
     pub module: PathBuf,
     pub since_slot: Option<u64>,
     pub until_slot: Option<u64>,
-    pub config: Option<serde_json::Value>,
+    pub config: Option<PathBuf>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
@@ -34,6 +34,21 @@ pub struct Config {
     pub ledger: ledgers::u5c::Config,
     pub workers: Vec<WorkerConfig>,
     pub logging: LoggingConfig,
+}
+
+fn load_worker_config(config_path: Option<PathBuf>) -> miette::Result<serde_json::Value> {
+    match config_path {
+        Some(path) => {
+            let config_str = std::fs::read_to_string(&path)
+                .into_diagnostic()
+                .context("reading worker config file")?;
+
+            serde_json::from_str::<serde_json::Value>(&config_str)
+                .into_diagnostic()
+                .context("parsing worker config as JSON")
+        }
+        None => Ok(serde_json::Value::Null),
+    }
 }
 
 #[tokio::main]
@@ -60,8 +75,10 @@ async fn main() -> miette::Result<()> {
         .context("setting up runtime")?;
 
     for worker in config.workers {
+        let config = load_worker_config(worker.config)?;
+
         runtime
-            .register_worker(&worker.name, worker.module, worker.config)
+            .register_worker(&worker.name, worker.module, config)
             .await
             .into_diagnostic()
             .context("registering worker")?;
