e82144142016117a495708682f24af5fc5b2b271
Santiago Carmuega <santiago@carmuega.me>
2024-11-06 09:06:01 -0300

    fix config

--- balius-runtime/tests/u5c-chainsync.rs
+++ balius-runtime/tests/u5c-chainsync.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 #![cfg(feature = "utxorpc")]
 
-use balius_runtime::{drivers, ledgers, Runtime, Store};
+use balius_runtime::{drivers, Runtime, Store};
 use serde_json::json;
 use tokio_util::sync::CancellationToken;
 
@@ -12,14 +12,7 @@ async fn wallet_balance() {
     let mut runtime = Runtime::builder(store).build().unwrap();
 
     let config = json!({
-      "validator": {
-        "ref_txo": {
-          "transaction_id": "f7d3837715680f3a170e99cd202b726842d97f82c05af8fcd18053c64e33ec4f",
-          "index": 0
-        },
-        "hash": "ef7a1cebb2dc7de884ddf82f8fcbc91fe9750dcd8c12ec7643a99bbe",
         "address": "addr1qx2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3n0d3vllmyqwsx5wktcd8cc3sq835lu7drv2xwl2wywfgse35a3x"
-      }
     });
 
     runtime
