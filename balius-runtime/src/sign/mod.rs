80a4520b7f1575b5f4924ddbdbf8f2bcf3b153e5
Simon Gellis <simongellis@gmail.com>
2025-04-29 17:37:50 -0400

    feat: add signer

---
+++ balius-runtime/src/sign/mod.rs
@@ -0,0 +1,37 @@
+pub mod in_memory;
+
+use std::sync::Arc;
+
+use tokio::sync::Mutex;
+
+use crate::wit::balius::app::sign as wit;
+
+#[derive(Clone)]
+pub enum Signer {
+    InMemory(in_memory::Signer),
+    Custom(Arc<Mutex<dyn wit::Host + Send + Sync>>),
+}
+
+impl From<in_memory::Signer> for Signer {
+    fn from(signer: in_memory::Signer) -> Self {
+        Signer::InMemory(signer)
+    }
+}
+
+#[async_trait::async_trait]
+impl wit::Host for Signer {
+    async fn sign_payload(
+        &mut self,
+        key_name: String,
+        algorithm: String,
+        payload: wit::Payload,
+    ) -> Result<wit::Signature, wit::SignError> {
+        match self {
+            Self::InMemory(signer) => signer.sign_payload(&key_name, &algorithm, payload),
+            Self::Custom(signer) => {
+                let mut lock = signer.lock().await;
+                lock.sign_payload(key_name, algorithm, payload).await
+            }
+        }
+    }
+}
