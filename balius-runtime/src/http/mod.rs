cb87c1b34e0aaa543330444daeee0a139dc39db1
Simon Gellis <simongellis@gmail.com>
2025-05-06 18:19:55 -0400

    feat: add user-friendly wrapper around http interface

--- balius-runtime/src/http/mod.rs
+++ balius-runtime/src/http/mod.rs
@@ -12,7 +12,6 @@ pub enum Http {
     Reqwest(reqwest::Client),
 }
 
-
 #[async_trait]
 impl wit::Host for Http {
     async fn request(
@@ -33,10 +32,9 @@ impl wit::Host for Http {
                     Some(wit::Scheme::Other(scheme)) => &scheme,
                     None => "http"
                 };
-                let uri = match (request.authority, request.path_and_query) {
-                    (Some(authority), Some(path)) => format!("{scheme}://{}/{}", authority.trim_end_matches("/"), path),
+                let uri = match (request.authority.as_deref(), request.path_and_query.as_deref()) {
                     (None, None) => { return Err(wit::ErrorCode::HttpRequestUriInvalid) }
-                    (auth, path) => format!("{scheme}://{}", auth.or(path).unwrap())
+                    (auth, path) => format!("{scheme}://{}{}", auth.unwrap_or_default(), path.unwrap_or_default()),
                 };
 
                 let method = match request.method {
@@ -107,73 +105,3 @@ fn map_reqwest_response_err(e: reqwest::Error) -> wit::ErrorCode {
         wit::ErrorCode::InternalError(Some(message))
     }
 }
-
-/*
-impl Http {
-    async fn request(
-        &mut self,
-        method: Method,
-        url: String,
-        headers: Vec<Header>,
-        body: Option<Body>,
-    ) -> Result<Body, HttpError> {
-        match self {
-            Http::Mock => Ok(vec![]),
-            Http::Reqwest(client) => {
-                let mut header_map = HeaderMap::new();
-                for (key, value) in headers {
-                    let key = HeaderName::from_str(key.as_str()).map_err(|_| 1u32)?;
-                    let value = HeaderValue::from_str(value.as_str()).map_err(|_| 1u32)?;
-                    header_map.insert(key, value);
-                }
-                let mut req = client.request(method, url).headers(header_map);
-                if let Some(body) = body {
-                    req = req.body(body);
-                }
-                let req = req.build().map_err(|_| 1u32)?;
-                let resp = client
-                    .execute(req)
-                    .await
-                    .map_err(|e| e.status().map(|s| s.as_u16() as u32).unwrap_or(1u32))?;
-                let body = resp.bytes().await.map_err(|_| 1u32)?.into();
-
-                Ok(body)
-            }
-        }
-    }
-}
-*/
-
-//#[async_trait]
-//impl wit::Host for Http {
-/*
-    async fn get(&mut self, url: String, headers: Vec<Header>) -> Result<Body, HttpError> {
-        self.request(Method::GET, url, headers, None).await
-    }
-
-    async fn post(
-        &mut self,
-        url: String,
-        headers: Vec<Header>,
-        body: Body,
-    ) -> Result<Body, HttpError> {
-        self.request(Method::POST, url, headers, Some(body)).await
-    }
-    async fn patch(
-        &mut self,
-        url: String,
-        headers: Vec<Header>,
-        body: Body,
-    ) -> Result<Body, HttpError> {
-        self.request(Method::PATCH, url, headers, Some(body)).await
-    }
-    async fn delete(
-        &mut self,
-        url: String,
-        headers: Vec<Header>,
-        body: Body,
-    ) -> Result<Body, HttpError> {
-        self.request(Method::DELETE, url, headers, Some(body)).await
-    }
-}
- */
