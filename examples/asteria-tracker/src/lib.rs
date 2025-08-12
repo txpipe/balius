c43c5b588217b29f75683dc307a7a85c9e745c08
Nicolás Ludueña <luduena.nicolas.victorio@gmail.com>
2025-08-12 19:00:00 -0300

    feat: add utils for parsing plutus data string and int

--- examples/asteria-tracker/src/lib.rs
+++ examples/asteria-tracker/src/lib.rs
@@ -2,20 +2,40 @@ use balius_sdk::wit::balius::app as worker;
 use balius_sdk::{self as sdk};
 
 use serde::{Deserialize, Serialize};
+use utxorpc_spec::utxorpc::v1alpha::cardano::plutus_data::{self};
+use utxorpc_spec::utxorpc::v1alpha::cardano::{big_int, BigInt, PlutusData};
 
 #[derive(Serialize, Deserialize, Clone)]
 struct SomeConfig {
     custom_hello: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct SayHelloParams {
-    name: String,
+fn string_plutus_field(p: Option<&PlutusData>) -> Option<Vec<u8>> {
+    match p {
+        Some(x) => match x.plutus_data.clone() {
+            Some(plutus_data::PlutusData::BoundedBytes(x)) => Some(x.to_vec()),
+            _ => None,
+        },
+        _ => None,
+    }
 }
 
-#[derive(Serialize, Deserialize)]
-struct Reply {
-    message: String,
+fn integer_plutus_field(p: Option<&PlutusData>) -> Option<i64> {
+    match p {
+        Some(x) => match x.plutus_data.clone() {
+            Some(plutus_data::PlutusData::BigInt(x)) => {
+                let bigint = x.big_int;
+                match bigint {
+                    Some(big_int::BigInt::Int(x)) => Some(x),
+                    Some(big_int::BigInt::BigUInt(_)) => None,
+                    Some(big_int::BigInt::BigNInt(_)) => None,
+                    _ => None,
+                }
+            }
+            _ => None,
+        },
+        _ => None,
+    }
 }
 
 #[derive(Serialize, Deserialize)]
