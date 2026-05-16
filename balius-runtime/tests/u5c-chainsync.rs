e3ed784e3ce0feb676be3c4ef9a3f287f7469fa3
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2026-05-16 12:44:32 -0300

    feat: support custom stores via trait interface (#67)

--- balius-runtime/tests/u5c-chainsync.rs
+++ balius-runtime/tests/u5c-chainsync.rs
@@ -2,13 +2,13 @@
 
 use std::collections::HashMap;
 
-use balius_runtime::{drivers, Runtime, Store};
+use balius_runtime::{drivers, store::redb::Store as RedbStore, Runtime, Store};
 use serde_json::json;
 use tokio_util::sync::CancellationToken;
 
 #[tokio::test]
 async fn wallet_balance() {
-    let store = Store::open("tests/balius.db", None).unwrap();
+    let store = Store::Redb(RedbStore::open("tests/balius.db", None).unwrap());
     let runtime = Runtime::builder(store).build().unwrap();
     let config = json!({
         "address": "addr1qx2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3n0d3vllmyqwsx5wktcd8cc3sq835lu7drv2xwl2wywfgse35a3x"
